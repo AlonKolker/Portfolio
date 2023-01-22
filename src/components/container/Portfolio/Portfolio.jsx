5d7f546d2d3b0d80fa5e4e43971e832d4293bc6d
Alon Kolker <alonkolk@gmail.com>
2023-01-22 13:59:26 +0200

    waerIt project was added

--- src/components/container/Portfolio/Portfolio.jsx
+++ src/components/container/Portfolio/Portfolio.jsx
@@ -29,7 +29,7 @@ const Portfolio = () => {
   }
 
   return (
-    <div className="container" id="projects">
+    <div className="container" id="portfolio">
       <motion.div
         initial={{opacity: 0}}
         whileInView={{y: [-50, 0], opacity: 1}}
